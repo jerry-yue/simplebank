fb941bebf669ae945388a9c7c630bbde44f29dfb
Jerry Yue <jerry_yue@trendmicro.com>
2022-06-06 15:41:32 +0800

    add viper config file

--- main.go
+++ main.go
@@ -6,16 +6,24 @@ import (
 
 	"github.com/jerry-yue/simplebank/api"
 	db "github.com/jerry-yue/simplebank/db/sqlc"
+	"github.com/jerry-yue/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:P@ssw0rd@127.0.0.1:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// viper insteaded!!!
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:P@ssw0rd@127.0.0.1:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
+// viper insteaded!!!
 
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Load config file failed: ", err)
+	}
+
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Connect to db failed: ", err)
@@ -24,7 +32,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Server started failed: ", err)
 	}
