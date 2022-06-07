cd0ab3a9903cb16e23eb70f2e15bab539e08eabd
Jerry Yue <jerry_yue@trendmicro.com>
2022-06-07 17:25:13 +0800

    Paseto and JWT feature worked! Now is Paseto. If want to change to JWT, just update NewServer func's to token.NewJWTMaker.That's all done

--- api/user.go
+++ api/user.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 	"time"
 
@@ -18,12 +19,22 @@ type createUserRequest struct {
 }
 
 // remove the password response from HTTP restful API
-type createUserReponse struct {
+type userReponse struct {
 	Username          string    `json:"username"`
 	FullName          string    `json:"full_name"`
 	Email             string    `json:"email"`
 	PasswordChangedAt time.Time `json:"password_changed_at"`
 	CreatedAt         time.Time `json:"created_at"`
+}
+
+func newUserReponse(user db.User) userReponse {
+	return userReponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		PasswordChangedAt: user.PasswordChangedAt,
+		Email:             user.Email,
+		CreatedAt:         user.CreatedAt,
+	}
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -60,13 +71,56 @@ func (server *Server) createUser(ctx *gin.Context) {
 	}
 
 	// remove the password response from HTTP restful API
-	rsp := createUserReponse{
-		Username:          user.Username,
-		FullName:          user.FullName,
-		PasswordChangedAt: user.PasswordChangedAt,
-		Email:             user.Email,
-		CreatedAt:         user.CreatedAt,
+	rsp := newUserReponse(user)
+
+	ctx.JSON(http.StatusOK, rsp)
+}
+
+type LoginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type LoginUserReponse struct {
+	AccessToken string      `json:"access_token"`
+	User        userReponse `json:"user"`
+}
+
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req LoginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
 	}
 
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	accessToken, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	rsp := LoginUserReponse{
+		AccessToken: accessToken,
+		User:        newUserReponse(user),
+	}
 	ctx.JSON(http.StatusOK, rsp)
 }
