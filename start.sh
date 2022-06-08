b15a348a97fdd175c270a4cb690a59c19233f4a3
Jerry Yue <57942521+jerry-yue@users.noreply.github.com>
2022-06-09 01:15:04 +0800

    Ft/docker (#6)
    * deploy workflow: add check out code step
    
    * update app.env via aws secret manager
    
    * fix db_srouce var issue in start.sh file
    
    Co-authored-by: Jerry Yue <jerry_yue@trendmicro.com>

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "check migrate file"
 /app/migrate -version
 
 echo "run db migration"
-
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
